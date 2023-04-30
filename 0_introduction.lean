850eefec9015322a78f1c2c0719b86dac8d9af94
acnq <fanyuanhaotc@163.com>
2023-05-01 02:24:06 +0300

    change to p3

---
+++ 0_introduction.lean
@@ -0,0 +1,5 @@
+def add1 (n : Nat) : Nat := n + 1
+
+#eval add1 7
+
+
---
+++ 1.1_eval_expr.lean
@@ -0,0 +1,10 @@
+
+#eval 1 + 2
+
+#eval 1 + 2 * 5
+
+open String
+#eval String.append "Hello, " "Lean!"
+
+#eval String.append "great " (String.append "oak " "tree")
+
