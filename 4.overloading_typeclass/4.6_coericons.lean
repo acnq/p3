588ced10ddad9cae41c86aed5a23b34baca78f2b
acnq <fanyuanhaotc@163.com>
2023-05-27 02:51:14 +0300

    adding content

--- 4.overloading_typeclass/4.6_coericons.lean
+++ 4.overloading_typeclass/4.6_coericons.lean
@@ -1,15 +1,22 @@
--- Coericions
+-- Coericions //强转
 
 -- Postive Numbers
 
 inductive Pos : Type where
   | one : Pos
   | succ : Pos → Pos
-
 def Pos.toNat : Pos → Nat 
   | Pos.one => 1
   | Pos.succ n => n.toNat + 1
-
+instance : ToString Pos where
+  toString x := toString (x.toNat)
+instance : OfNat Pos (n + 1) where
+  ofNat := 
+    let rec natPlusOne : Nat → Pos
+      | 0 => Pos.one
+      | k + 1 => Pos.succ (natPlusOne k)
+    natPlusOne n 
+#eval (3 : Pos)
 -- #eval [1, 2, 3, 4].drop (2 : Pos)
 -- application type mismatch
 --   List.drop 2
@@ -19,3 +26,57 @@ def Pos.toNat : Pos → Nat
 --   Pos : Type
 -- but is expected to have type
 --   Nat : Type
+
+namespace Hidden
+class Coe (α : Type) (β : Type) where 
+  coe : α → β  
+end Hidden
+
+instance : Coe Pos Nat where
+  coe x := x.toNat
+
+#eval [1, 2, 3, 4].drop (2 : Pos)
+
+-- Chaining Coericons
+def oneInt : Int := Pos.one
+
+inductive A where
+  | a 
+
+inductive B where
+  | b 
+deriving Repr
+
+instance : Coe A B where
+  coe _ := B.b
+
+instance : Coe B A where
+  coe _ := A.a
+
+instance : Coe Unit A where
+  coe _ := A.a
+
+def coercedToB : B := () -- () used for Unit.unit 
+#eval coercedToB
+
+def List.last? : List α → Option α 
+  | [] => none
+  | [x] => x
+  | _ :: x :: xs => last? (x :: xs)
+
+def perhapsPerhapsPerhaps : Option (Option (Option String)) := 
+  "Please don't tell me"
+
+-- def perhapsPerhapsPerhapsNat : Option (Option (Option Nat)) := 
+--   392
+-- -- failed to synthesize instance
+--   OfNat (Option (Option (Option Nat))) 392
+-- since it is a instance missing, not a type mismatch, the coericon is not of help
+
+def perhapsPerhapsPerhapsNat : Option (Option (Option Nat)) := 
+  (392 : Nat)
+
+def perhapsPerhapsPerhapsNat' : Option (Option (Option Nat)) := 
+  ↑(392 : Nat)
+-- ↑ as coericon 
+
