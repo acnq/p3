b5a246d737eb5392e70dc98a0ac61dd2a1abea39
acnq <fanyuanhaotc@163.com>
2023-05-28 00:37:51 +0300

    finish 4.6

--- 4.overloading_typeclass/4.6_coericons.lean
+++ 4.overloading_typeclass/4.6_coericons.lean
@@ -1,3 +1,4 @@
+import Lean
 -- Coericions //强转
 
 -- Postive Numbers
@@ -134,5 +135,121 @@ instance : CoeSort Bool Prop where
   coe b := b = true
 
 -- Coercing to Functions
+namespace Hidden
+class CoeFun (α : Type) (makeFunctionType : outParam (α → Type)) where
+  coe : (x : α) → makeFunctionType x 
+end Hidden
 
+structure Adder where
+  howMuch : Nat
 
+def add5 : Adder := ⟨5⟩
+
+-- it is not a function
+-- #eval add5 3 
+-- function expected at
+--   add5
+-- term has type
+--   Adder
+-- we need CoeFun
+
+instance : CoeFun Adder (fun _ => Nat → Nat) where
+  coe a := (· + a.howMuch)
+
+#eval add5 3 
+
+inductive JSON where
+  | true : JSON
+  | false : JSON
+  | null : JSON
+  | string : String → JSON
+  | number : Float → JSON
+  | object : List (String × JSON) → JSON
+  | array : List JSON → JSON
+deriving Repr
+
+structure Serializer where
+  Contents : Type 
+  serialize : Contents →  JSON
+
+def Str : Serializer := 
+  {Contents := String,
+    serialize := JSON.string
+  }
+
+instance : CoeFun Serializer (fun s => s.Contents → JSON) where
+  coe s := s.serialize
+
+def buildResponse (title : String) (R : Serializer) (record : R.Contents) : JSON :=
+  JSON.object [
+    ("title", JSON.string title),
+    ("status", JSON.number 200),
+    ("record", R record)
+  ]
+
+#eval buildResponse "Functional Programming in Lean" Str "Programming is fun!"
+
+-- Aside : JSON as a String
+#eval (5 : Float).toString
+
+def dropDecimals (numString : String) : String := 
+  if numString.contains '.' then
+    let noTrailingZeros := numString.dropRightWhile (· == '0')
+    noTrailingZeros.dropRightWhile (· == '.')
+  else numString
+#eval dropDecimals (5 : Float).toString
+#eval dropDecimals (5.2 : Float).toString
+
+def String.separate (sep : String) (strings : List String) : String :=
+  match strings with
+    | [] => ""
+    | x :: xs => String.join (x :: xs.map (sep ++ ·))
+
+#eval ", ".separate ["1", "2"]
+#eval ", ".separate ["1"]
+#eval ", ".separate []
+
+partial def JSON.asString (val : JSON) : String := 
+  match val with 
+    | true => "true"
+    | false => "false"
+    | null => "null"
+    | string s => "\"" ++ Lean.Json.escape s ++ "\""
+    | number n => dropDecimals n.toString
+    | object members => 
+      let memberToString mem := 
+        "\"" ++ Lean.Json.escape mem.fst ++ "\": " ++ asString mem.snd  
+      "{" ++ ", ".separate (members.map memberToString) ++ "}"
+    | array elements =>
+      "[" ++  ",".separate (elements.map asString) ++ "]"
+
+#check Lean.Json.escape
+#eval (buildResponse "Functional Programming in Lean" Str "Programming is fun!").asString
+
+-- Design Considerations
+def idahoSpiders : NonEmptyList String := {
+  head := "Banded Garden Spider",
+  tail := [
+    "Long-legged Sac Spider",
+    "Wolf Spider",
+    "Hobo Spider",
+    "Cat-faced 蜘蛛"
+  ]
+}  
+
+def lastSpider : Option String :=
+  List.getLast? idahoSpiders
+
+-- However, if type annotation is omitted, 
+-- the result type is unkown, so Lean is unable to find the coericons
+-- def lastSpider' :=
+--   List.getLast? idahoSpiders
+
+-- application type mismatch
+--   List.getLast? idahoSpiders
+-- argument
+--   idahoSpiders
+-- has type
+--   NonEmptyList String : Type
+-- but is expected to have type
+--   List ?m.16727 : Type
